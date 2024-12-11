30a01136492089ead8e272d47ecdda7881789837
John Doe <johndoe@example.com>
2024-12-11 15:29:26 +0300

    mega new one commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, redirect
+import The_smart_part
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/add')
+def buy():
+    return render_template('add.html')
+
+
+@app.route('/korzina')
+def abort():
+    return render_template('korzina.html')
+
+
+if __name__ == '__main__':
+    app.run(port=60000, host='0.0.0.0')
