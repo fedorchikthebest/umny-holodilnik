1b2832ebc43586c6ded22880fa6adaf27a29236d
kharitonovdo <johndoe@example.com>
2024-12-14 13:52:44 +0300

    test commit

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 # Список для хранения товаров (можно позже заменить на базу данных)
 products = []
 
+
 @app.route('/')
 def index():
     return render_template('index.html', products=products)
