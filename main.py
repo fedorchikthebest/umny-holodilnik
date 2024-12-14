56d8f5ebc828fa6bd1f2810d3628653681fb56ee
kharitonovdo <kharitonovdo@yandex.ru>
2024-12-14 13:42:32 +0300

    adt

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+# Список для хранения товаров (можно позже заменить на базу данных)
+products = []
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', products=products)
 
-
-@app.route('/add')
-def buy():
+@app.route('/add', methods=['GET', 'POST'])
+def add_product():
+    if request.method == 'POST':
+        name = request.form['name']
+        price = request.form['price']
+        description = request.form['description']
+        
+        product = {
+            'name': name,
+            'price': price,
+            'description': description
+        }
+        products.append(product)
+        return redirect(url_for('index'))
+    
     return render_template('add.html')
 
-
 @app.route('/korzina')
-def abort():
-    return render_template('korzina.html')
-
+def korzina():
+    return render_template('korzina.html', products=products)
 
 if __name__ == '__main__':
-    app.run(port=60000, host='0.0.0.0')
+    app.run(debug=True)
