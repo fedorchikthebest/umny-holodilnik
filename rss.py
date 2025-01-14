d3f6b1a0074f316efc5b2a7d8cafb6bd96f01e13
Fedor Gorlenko <gimonchik@gimonchik.ru>
2025-01-14 18:53:20 +0000

    rss beta fix

--- rss.py
+++ rss.py
@@ -2,7 +2,7 @@ from db_operations import *
 import datetime
 
 
-RSS_START = '<?xml version="1.0" encoding="UTF-8"?>\n<rss version="2.0">\n<channel>\n<title>FreeBSD in the Press</title>\n<link>https://www.FreeBSD.org/press/</link>\n<description>Press Stories about FreeBSD</description>\n<link>https://www.opennet.me/opennews/</link>\n'
+RSS_START = '<?xml version="1.0" encoding="UTF-8"?>\n<rss version="2.0">\n<channel>\n<title>FreeBSD in the Press</title>\n<link>https://www.FreeBSD.org/press/</link>\n<description>Press Stories about FreeBSD</description>\nn'
 RSS_END = "</channel></rss>"
 RSS_SHAB = "<item><title>Срок годности истек</title>\n<link>https://www.opennet.me/opennews/art.shtml?num=62503</link>\n<pubDate>Thu, 02 Jan 2025 11:08:19 +0300</pubDate>\n<description>Товар %s испортился</description></item>"
 
