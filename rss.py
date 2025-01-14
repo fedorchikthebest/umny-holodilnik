638e923c730bf78aba839860c0bfbda6ff1e181a
Fedor Gorlenko <gimonchik@gimonchik.ru>
2025-01-14 19:00:52 +0000

    rss beta fix

--- rss.py
+++ rss.py
@@ -2,9 +2,9 @@ from db_operations import *
 import datetime
 
 
-RSS_START = '<?xml version="1.0" encoding="UTF-8"?>\n<rss version="2.0">\n<channel>\n<title>FreeBSD in the Press</title>\n<link>https://www.FreeBSD.org/press/</link>\n<description>Press Stories about FreeBSD</description>\nn'
+RSS_START = '<?xml version="1.0" encoding="UTF-8"?>\n<rss version="2.0">\n<channel>\n<title>Информация о продуктаз</title>\n<link>https://www.FreeBSD.org/press/</link>\n<description>Тут будет размещаться информация о состоянии товаров в холодильнике</description>\nn'
 RSS_END = "</channel></rss>"
-RSS_SHAB = "<item><title>Срок годности истек</title>\n<pubDate>Thu, 02 Jan 2025 11:08:19 +0300</pubDate>\n<description>Товар %s испортился</description></item>"
+RSS_SHAB = "<item><title>Срок годности истек</title>\n<description>Товар %s испортился</description></item>"
 
 
 def gen_rss() -> str:
