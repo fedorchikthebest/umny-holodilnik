36f3f3fe3b3b41910781f8d44a6719db7d9741b7
Fedor Gorlenko <gimonchik@gimonchik.ru>
2025-01-14 19:26:14 +0000

    rss implementation

--- rss.py
+++ rss.py
@@ -2,7 +2,7 @@ from db_operations import *
 import datetime
 
 
-RSS_START = '<?xml version="1.0" encoding="UTF-8"?>\n<rss version="2.0">\n<channel>\n<title>Информация о продуктаз</title>\n<link>https://www.FreeBSD.org/press/</link>\n<description>Тут будет размещаться информация о состоянии товаров в холодильнике</description>\nn'
+RSS_START = '<?xml version="1.0" encoding="UTF-8"?>\n<rss version="2.0">\n<channel>\n<title>Информация о продуктаз</title>\n<description>Тут будет размещаться информация о состоянии товаров в холодильнике</description>\nn'
 RSS_END = "</channel></rss>"
 RSS_SHAB = "<item><title>%s</title>\n<pubDate>%s</pubDate>\n<description>%s</description></item>"
 
