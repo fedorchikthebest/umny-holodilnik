af1156c918bf42753d825f8a329afb7cdd6adc26
Fedor Gorlenko <gimonchik@gimonchik.ru>
2025-01-25 22:22:33 +0300

    added charts to analytics

--- static/js/analytics.js
+++ static/js/analytics.js
@@ -3,18 +3,32 @@ const start_date = document.getElementById("startDate")
 const stop_date = document.getElementById("endDate")
 let circle_chart = document.getElementById("circleChart")
 let p_chart = document.getElementById("potreblenieChart")
+let d_chart = document.getElementById("deleteChart")
 
 const pc = new Chart(p_chart, {
-    type: 'doughnut',
+    type: 'bar',
     data: {
       labels: [],
       datasets: [{
         label: 'Хранится',
-        data: [0, 0],
+        data: [],
         borderWidth: 1
       }]
     },
   });
+
+const dc = new Chart(d_chart, {
+    type: 'bar',
+    data: {
+      labels: [],
+      datasets: [{
+        label: 'Удалено',
+        data: [],
+        borderWidth: 1
+      }]
+    },
+  });
+
 
 const cc = new Chart(circle_chart, {
     type: 'doughnut',
@@ -42,8 +56,21 @@ async function create_chart(){
 	cc.data.datasets[0].data = [res_t.deleted.length, res_t.products.length]
 	cc.update()
 
-	let r1 = Set()
-	r1.forEach((e) => r1.add(e.name))
+	let r1 = new Set()
+	let d1 = new Set()
+
+	res_t.products.forEach((e) => r1.add(e.product_name))
+	res_t.deleted.forEach((e) => d1.add(e.product_name))
+
+	pc.data.datasets[0].data = []
+	r1.forEach((e) => pc.data.datasets[0].data.push(res_t.products.filter(x => x.product_name === e).length))
+	pc.data.labels = Array.from(r1)
+	pc.update()
+
+	dc.data.datasets[0].data = []
+	d1.forEach((e) => dc.data.datasets[0].data.push(res_t.deleted.filter(x => x.product_name === e).length))
+	dc.data.labels = Array.from(d1)
+	dc.update()
 }
 
 btn.addEventListener('click', () => create_chart())
