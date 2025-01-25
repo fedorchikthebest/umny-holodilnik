ed7f85b0b87e950fa0c81a29367af77d37c67f84
Fedor Gorlenko <fedorgorlenko@gmail.com>
2025-01-25 20:36:03 +0300

    base analytycs

---
+++ static/js/analytics.js
@@ -0,0 +1,33 @@
+const btn = document.getElementById("generateReport")
+const start_date = document.getElementById("startDate")
+const stop_date = document.getElementById("endDate")
+const circle_chart = document.getElementById("circleChart")
+
+const cc = new Chart(circle_chart, {
+    type: 'doughnut',
+    data: {
+      labels: ['Удалённые', 'Существующие'],
+      datasets: [{
+        label: '# of Votes',
+        data: [0, 0],
+        borderWidth: 1
+      }]
+    },
+  });
+
+
+function to_int_time(t){
+	let e = t.split('-')
+	ans = new Date(e[0], e[1] - 1, e[2])
+	return ans.getTime() / 1000
+}
+
+
+async function create_chart(){
+	let res = await fetch('/api/get_analytics/' + to_int_time(start_date.value) + '/' + to_int_time(stop_date.value))
+	let res_t = await res.json()
+	cc.data.datasets[0] = [res_t.deleted.length, res_t.products.length]
+	cc.update()
+}
+
+btn.addEventListener('click', () => create_chart())
