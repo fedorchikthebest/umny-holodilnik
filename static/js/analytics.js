ff8fd448766279872f70a059a2f4d88b743cc1de
Alena41 <123312053+Alena41@users.noreply.github.com>
2025-01-26 13:06:34 +0300

    Update analytics.js

--- static/js/analytics.js
+++ static/js/analytics.js
@@ -1,76 +1,78 @@
-const btn = document.getElementById("generateReport")
-const start_date = document.getElementById("startDate")
-const stop_date = document.getElementById("endDate")
-let circle_chart = document.getElementById("circleChart")
-let p_chart = document.getElementById("potreblenieChart")
-let d_chart = document.getElementById("deleteChart")
+const btn = document.getElementById("generateReport");
+const start_date = document.getElementById("startDate");
+const stop_date = document.getElementById("endDate");
+let circle_chart = document.getElementById("circleChart");
+let p_chart = document.getElementById("potreblenieChart");
+let d_chart = document.getElementById("deleteChart");
+
+const colors = ['#FF6384', '#36A2EB'];
 
 const pc = new Chart(p_chart, {
     type: 'bar',
     data: {
-      labels: [],
-      datasets: [{
-        label: 'Хранится',
-        data: [],
-        borderWidth: 1
-      }]
+        labels: [],
+        datasets: [{
+            label: 'Хранится',
+            data: [],
+            backgroundColor: colors[1],
+            borderWidth: 1
+        }]
     },
-  });
+});
 
 const dc = new Chart(d_chart, {
     type: 'bar',
     data: {
-      labels: [],
-      datasets: [{
-        label: 'Удалено',
-        data: [],
-        borderWidth: 1
-      }]
+        labels: [],
+        datasets: [{
+            label: 'Удалено',
+            data: [],
+            backgroundColor: colors[0],
+            borderWidth: 1
+        }]
     },
-  });
-
+});
 
 const cc = new Chart(circle_chart, {
     type: 'doughnut',
     data: {
-      labels: ['Удалённые', 'Существующие'],
-      datasets: [{
-        label: 'Продукты',
-        data: [0, 0],
-        borderWidth: 1
-      }]
+        labels: ['Удалённые', 'Существующие'],
+        datasets: [{
+            label: 'Продукты',
+            data: [0, 0],
+            backgroundColor: colors,
+            borderWidth: 1
+        }]
     },
-  });
+});
 
-
-function to_int_time(t){
-	let e = t.split('-')
-	ans = new Date(e[0], e[1] - 1, e[2])
-	return ans.getTime() / 1000
+function to_int_time(t) {
+    let e = t.split('-');
+    ans = new Date(e[0], e[1] - 1, e[2]);
+    return ans.getTime() / 1000;
 }
 
+async function create_chart() {
+    let res = await fetch('/api/get_analytics/' + to_int_time(start_date.value) + '/' + to_int_time(stop_date.value));
+    let res_t = await res.json();
+    cc.data.datasets[0].data = [res_t.deleted.length, res_t.products.length];
+    cc.update();
 
-async function create_chart(){
-	let res = await fetch('/api/get_analytics/' + to_int_time(start_date.value) + '/' + to_int_time(stop_date.value))
-	let res_t = await res.json()
-	cc.data.datasets[0].data = [res_t.deleted.length, res_t.products.length]
-	cc.update()
+    let r1 = new Set();
+    let d1 = new Set();
 
-	let r1 = new Set()
-	let d1 = new Set()
+    res_t.products.forEach((e) => r1.add(e.product_name));
+    res_t.deleted.forEach((e) => d1.add(e.product_name));
 
-	res_t.products.forEach((e) => r1.add(e.product_name))
-	res_t.deleted.forEach((e) => d1.add(e.product_name))
+    pc.data.datasets[0].data = [];
+    r1.forEach((e) => pc.data.datasets[0].data.push(res_t.products.filter(x => x.product_name === e).length));
+    pc.data.labels = Array.from(r1);
+    pc.update();
 
-	pc.data.datasets[0].data = []
-	r1.forEach((e) => pc.data.datasets[0].data.push(res_t.products.filter(x => x.product_name === e).length))
-	pc.data.labels = Array.from(r1)
-	pc.update()
-
-	dc.data.datasets[0].data = []
-	d1.forEach((e) => dc.data.datasets[0].data.push(res_t.deleted.filter(x => x.product_name === e).length))
-	dc.data.labels = Array.from(d1)
-	dc.update()
+    dc.data.datasets[0].data = [];
+    d1.forEach((e) => dc.data.datasets[0].data.push(res_t.deleted.filter(x => x.product_name === e).length));
+    dc.data.labels = Array.from(d1);
+    dc.update();
 }
 
-btn.addEventListener('click', () => create_chart())
+btn.addEventListener('click', () => create_chart());
